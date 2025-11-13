68ed0eb8f59f51a330cdf98340793166e0d881c1
Sharfuddin <sharfu6474@gmail.com>
2025-11-14 03:21:57 +0530

    Add files via upload

---
+++ scripts/EDA_mysql/EDA_Queries.sql
@@ -0,0 +1,160 @@
+-- 1.Overall Performance:
+--  What are the total sales revenue and total quantity sold?
+SELECT 
+	SUM(sls_sales) AS total_sales_revenue,
+    SUM(sls_quantity) AS total_quantity
+FROM fact_sale;
+
+-- What is the total number of unique orders and unique customers?
+SELECT 
+	COUNT(DISTINCT sls_ord_num) AS unique_order,
+	COUNT( DISTINCT cst_key) AS unique_customer
+FROM fact_sale;
+
+-- What is the overall average order value?
+SELECT 
+	ROUND(SUM(sls_sales) / COUNT(DISTINCT sls_ord_num),2) AS avg_order_value
+FROM fact_sale;
+
+-- 2. Time-Based Analysis:
+-- What are the total sales per year and per month?
+SELECT 
+	YEAR(sls_order_dt) AS order_year,
+    MONTHNAME(sls_order_dt) AS order_month,
+	SUM(sls_sales) AS total_sales
+FROM fact_sale
+GROUP BY YEAR(sls_order_dt), MONTHNAME(sls_order_dt)
+ORDER BY order_year , order_month;
+
+-- Is there a clear seasonality in sales?
+-- * From the above table its clear that from june sales is going high till december.
+
+-- What was the best month for sales?
+SELECT 
+	YEAR(sls_order_dt) AS order_year,
+    MONTHNAME(sls_order_dt) AS order_month,
+    SUM(sls_Sales) AS total_sales
+FROM fact_sale
+GROUP BY order_year,order_month
+ORDER BY total_sales DESC
+LIMIT 1;
+
+-- 3.Product Analysis:
+-- What are the top 10 best-selling products by revenue?
+SELECT 
+	p.prd_name AS best_selling_prd,
+	SUM(s.sls_sales) AS revenue
+FROM fact_sale s
+LEFT JOIN dim_product p
+ON s.prd_key = p.prd_key
+GROUP BY best_selling_prd
+ORDER BY revenue DESC
+LIMIT 10;
+
+-- What are the top 5 best-selling product categories and subcategories?
+SELECT 
+	p.category,
+    p.sub_category,
+    SUM(s.sls_sales) AS revenue
+FROM dim_product p
+LEFT JOIN fact_sale s 
+ON p.prd_key = s.prd_key
+GROUP BY category, sub_category
+ORDER BY  revenue DESC
+LIMIT 5;
+
+-- Which products or categories have the highest maintenance requirements? Does this correlate with sales?
+SELECT 
+    p.prd_name,
+    p.category,
+    COUNT(*) AS maintenance_count,
+    SUM(s.sls_sales) AS total_sales
+FROM dim_product p
+LEFT JOIN fact_sale s
+    ON p.prd_key = s.prd_key
+WHERE p.maintenance = 'Yes'
+GROUP BY p.prd_name, p.category
+ORDER BY maintenance_count DESC, total_sales DESC;
+
+-- 4.Customer Analysis:
+-- Who are the top 10 customers by total sales revenue?
+SELECT 
+	CONCAT(c.cst_firstname," ",c.cst_lastname) AS cst_name,
+    SUM(s.sls_sales) AS total_sales_revenue
+FROM dim_customer c
+LEFT JOIN fact_sale s 
+ON c.cst_key = s.cst_key
+GROUP BY  cst_name
+ORDER BY total_sales_revenue DESC
+LIMIT 10 ;
+
+-- What is the distribution of customers by country?
+SELECT 
+    cst_country,
+    COUNT(cst_key) AS total_customer
+FROM dim_customer
+GROUP BY cst_country
+ORDER BY total_customer DESC;
+
+-- What is the total sales revenue by country?
+SELECT 
+	c.cst_country AS country,
+    SUM(s.sls_sales) AS total_sales_revenue
+FROM dim_customer c
+LEFT JOIN fact_sale s
+ON c.cst_key = s.cst_key
+GROUP BY country
+ORDER BY total_sales_revenue DESC;
+
+-- 5.Customer Demographics:
+-- What is the sales breakdown by gender?
+SELECT 
+	c.cst_gender AS gender,
+    SUM(s.sls_sales) AS sales_revenue
+FROM dim_customer c
+LEFT JOIN fact_sale s
+ON c.cst_key = s.cst_key 
+GROUP BY gender
+ORDER BY sales_revenue DESC;
+
+-- What is the sales breakdown by marital status? (S vs. M).
+SELECT 
+	c.cst_marital_status AS marital_status,
+    SUM(s.sls_sales) AS total_sales_revenue
+FROM dim_customer c
+LEFT JOIN fact_sale s
+ON c.cst_key = s.cst_key 
+GROUP BY marital_status
+ORDER BY total_sales_revenue DESC;
+
+-- Calculate customer age: YEAR(CURDATE()) - YEAR(birth_date). What is the average age of your customers?
+SELECT 
+    AVG(TIMESTAMPDIFF(YEAR, cst_birthdate, CURDATE())) AS avg_cst_age
+FROM dim_customer;
+
+-- What is the sales distribution by age group (e.g., 20-30, 30-40, etc.)?
+SELECT 
+    CASE
+        WHEN TIMESTAMPDIFF(YEAR, c.cst_birthdate, CURDATE()) BETWEEN 20 AND 29 THEN '20-29'
+        WHEN TIMESTAMPDIFF(YEAR, c.cst_birthdate, CURDATE()) BETWEEN 30 AND 39 THEN '30-39'
+        WHEN TIMESTAMPDIFF(YEAR, c.cst_birthdate, CURDATE()) BETWEEN 40 AND 49 THEN '40-49'
+        WHEN TIMESTAMPDIFF(YEAR, c.cst_birthdate, CURDATE()) BETWEEN 50 AND 59 THEN '50-59'
+        ELSE '60+'
+    END AS age_group,
+    SUM(s.sls_sales) AS total_sales,
+    COUNT(s.sls_ord_num) AS total_orders,
+    COUNT(DISTINCT c.cst_key) AS total_customers
+FROM dim_customer c
+LEFT JOIN fact_sale s
+    ON c.cst_key = s.cst_key
+GROUP BY age_group
+ORDER BY age_group;
+
+
+
+
+
+
+
+
+
---
+++ scripts/EDA_mysql/creating_table_and_loading_data.sql
@@ -0,0 +1,84 @@
+-- creating database bikesales_db
+CREATE DATABASE bikeSales_db;
+
+USE bikeSales_db;
+
+-- creating dim_customer table
+CREATE TABLE dim_customer(
+cst_id INT,
+cst_number VARCHAR(50),
+cst_firstname VARCHAR(50),
+cst_lastname VARCHAR(50),
+cst_marital_status VARCHAR(50),
+cst_create_date DATE,
+cst_country VARCHAR(50),
+cst_birthdate DATE,
+cst_gender VARCHAR(50),
+cst_key INT PRIMARY KEY 
+);
+
+-- creating dim_product table
+CREATE TABLE dim_product(
+prd_id INT,
+prd_number VARCHAR(50),
+prd_name VARCHAR(50),
+prd_cost INT,
+prd_line VARCHAR(50),
+prd_start_dt DATE,
+cat_id INT,
+category VARCHAR(50),
+sub_category VARCHAR(50),
+maintenance VARCHAR(50),
+prd_key INT PRIMARY KEY
+);
+
+-- creating fact_sale table
+CREATE TABLE fact_sale(
+sls_ord_num VARCHAR(50),
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT,
+prd_key INT,
+cst_key INT,
+FOREIGN KEY(cst_key) REFERENCES dim_customer(cst_key),
+FOREIGN KEY(prd_key) REFERENCES dim_product(prd_key)
+);
+
+-- loading data in dim_customer
+LOAD DATA LOCAL INFILE "C:\\Users\\sharf\\OneDrive\\Desktop\\bike_sales_csv\\clean_dim_customer.xls"
+INTO TABLE dim_customer
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 ROWS;
+
+-- checking table
+SELECT * FROM dim_customer;
+select count(*) FROM dim_customer;
+
+-- loading data in dim_product
+LOAD DATA LOCAL INFILE "C:\\Users\\sharf\\OneDrive\\Desktop\\bike_sales_csv\\clean_dim_product.xls"
+INTO TABLE dim_product
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 ROWS;
+
+-- checking the table 
+SELECT * FROM dim_product;
+SELECT COUNT(*) FROM dim_product;
+
+-- loading data in dim_product
+LOAD DATA LOCAL INFILE "C:\\Users\\sharf\\OneDrive\\Desktop\\bike_sales_csv\\clean_fact_sales.xls"
+INTO TABLE fact_sale
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'  
+IGNORE 1 ROWS;
+
+-- checking the table 
+SELECT * FROM fact_sale;
+SELECT COUNT(*) FROM fact_sale;
